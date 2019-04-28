523801b816cc17ef03099dfc0d358e2984f6067a
ishitsukayuki <19961017yuki@gmail.com>
2019-04-28 16:46:14 +0900

    [Finish}

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,6 +30,7 @@ class BooksController < ApplicationController
 
 	def update
 		@book = Book.find(params[:id])
+		@book.user_id = current_user.id
 		if @book.update(book_params)
 			redirect_to book_path(@book), notice: "Book was successfuly update."
 		else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
   def index
     @users = User.all
     @user = current_user
@@ -7,12 +8,12 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-    @books = @user.books
     @book = Book.new
+    @books = @user.books
   end
 
   def edit
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
@@ -28,4 +29,8 @@ private
 	def user_params
 		params.require(:user).permit(:name, :profile_image, :profile)
 	end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(user_path(current_user))unless @user == current_user
+  end
 end
